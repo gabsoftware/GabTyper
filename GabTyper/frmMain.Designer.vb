501b815ade19c58d55acc435324f13dc1001c0b5
gabriel@gabsoftware.com <gabriel@gabsoftware.com>
2015-04-09 17:39:33 +0200

    initial commit

---
+++ GabTyper/frmMain.Designer.vb
@@ -0,0 +1,74 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
+        Me.txtText = New System.Windows.Forms.TextBox
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.mainTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'txtText
+        '
+        Me.txtText.AcceptsReturn = True
+        Me.txtText.Location = New System.Drawing.Point(12, 12)
+        Me.txtText.MaxLength = 65535
+        Me.txtText.Multiline = True
+        Me.txtText.Name = "txtText"
+        Me.txtText.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.txtText.Size = New System.Drawing.Size(812, 371)
+        Me.txtText.TabIndex = 0
+        Me.txtText.Text = resources.GetString("txtText.Text")
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 399)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(205, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "CTRL + SHIFT + T = start/stop typing text"
+        '
+        'mainTimer
+        '
+        Me.mainTimer.Interval = 150
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(837, 631)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtText)
+        Me.Name = "frmMain"
+        Me.Text = "GabTyper"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtText As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents mainTimer As System.Windows.Forms.Timer
+
+End Class
---
+++ GabTyper/frmMain.vb
@@ -0,0 +1,70 @@
+﻿Imports GabSoftware.Utils
+
+Public Class frmMain
+
+    ''' <summary>
+    ''' objet qui capture les touches du clavier
+    ''' </summary>
+    ''' <remarks></remarks>
+    Friend WithEvents keyHook As GabKeyboardHook
+
+    Private average As Integer = 87
+    Private variation As Integer = 175
+    Private minimum As Integer = 10
+    Private position As Integer = 0
+
+    Private Sub frmMain_FormClosing(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+        Me.keyHook.Stop(True, False)
+    End Sub
+
+    Private Sub frmMain_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'On capture toutes les touches même si on a pas le focus
+        Me.keyHook = New GabKeyboardHook(True)
+
+        Randomize()
+    End Sub
+
+    Private Sub keyHook_KeyDown(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles keyHook.KeyDown
+        Me.SetKeyboardActions(e, Me.ContainsFocus)
+    End Sub
+
+    Private Sub SetKeyboardActions(ByVal touche As System.Windows.Forms.KeyEventArgs, ByVal hasFocus As Boolean)
+        If touche.KeyCode = Keys.T And touche.Shift And touche.Control Then
+
+            If mainTimer.Enabled Then
+                mainTimer.Stop()
+            Else
+                Me.position = 0
+                mainTimer.Start()
+            End If
+
+
+        End If
+    End Sub
+
+    Private Sub mainTimer_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles mainTimer.Tick
+
+        Dim tmp As Integer
+        tmp = average + ((Rnd() * variation) - (variation / 2))
+        If tmp < minimum Then
+            tmp = minimum
+        End If
+        mainTimer.Interval = tmp
+
+        If position >= txtText.Text.Length Then
+            mainTimer.Stop()
+            Exit Sub
+        End If
+
+        SendKeys.Send(txtText.Text(position))
+
+        If txtText.Text(position) = vbCr Then
+            position += 1
+        End If
+        position += 1
+
+
+        'txtText.Text &= mainTimer.Interval.ToString() & vbNewLine
+
+    End Sub
+End Class
